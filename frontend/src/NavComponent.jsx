6b17bfaf297876d10f0f0adfcab5f7b43a1b02f2
kamilek7 <kamijar20031@gmail.com>
2025-08-30 13:16:00 +0200

    poprawa css

--- frontend/src/NavComponent.jsx
+++ frontend/src/NavComponent.jsx
@@ -8,7 +8,7 @@ const NavComponent = ({userID, wyloguj}) => {
         <div class='navigation'>
             <div class='hori-buttons'>
                 <button onClick={wyloguj}><i class='icon-logout'></i></button>
-                <button onClick={()=>{setModalState(1); setModalStateVisibilty(true);}}><i class='icon-plus-squared'></i></button>
+                <button class='mainBtn' onClick={()=>{setModalState(1); setModalStateVisibilty(true);}}><i class='icon-plus-squared'></i></button>
                 <button onClick={()=>{setModalState(2); setModalStateVisibilty(true);}}><i class='icon-user'></i></button>
             </div>
             <ModalWindow tryb={modalState} visibility={modalStateVisibility} setVisibility={setModalStateVisibilty} userID={userID}></ModalWindow>
